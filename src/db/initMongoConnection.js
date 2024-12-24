a400518a0a18840cdd1dfef3bcec525e49d0a3df
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-12-24 02:16:08 +0200

    7

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -2,10 +2,15 @@ import mongoose from 'mongoose';
 
 import { getEnvVar } from '../utils/getEnvVar.js';
 
-const dbHost = getEnvVar('DB_HOST');
-
 export const initMongoConnection = async () => {
   try {
+    const user = getEnvVar('MONGODB_USER');
+    const password = getEnvVar('MONGODB_PASSWORD');
+    const url = getEnvVar('MONGODB_URL');
+    const dbName = getEnvVar('MONGODB_DB');
+
+    const dbHost = `mongodb+srv://${user}:${password}@${url}/${dbName}?retryWrites=true&w=majority`;
+
     await mongoose.connect(dbHost);
     console.log('Mongo connection successfully established!');
   } catch (error) {
